612c482357975c62bbeeb2cc6a334e1ca3de5cd7
Larissa Karoline <larissakcarvalho@outlook.com>
2021-07-14 17:57:13 -0300

    Criação de classes

---
+++ Módulo 02 - Java básico/carro/src/Carro.java
@@ -0,0 +1,45 @@
+public class Carro {
+    
+    String marca;
+    String modelo;
+    int ano;
+    String variante;
+
+    public Carro (String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getVariante() {
+        return variante;
+    }
+
+    public void setVariante(String variante) {
+        this.variante = variante;
+    }
+}
